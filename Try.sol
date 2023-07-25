5487f9920106959002922257854b2dc9d7c77947
Manimannan93 <137152873+Manimannan93@users.noreply.github.com>
2023-07-25 22:14:15 +0530

    Update Try.sol

--- Try.sol
+++ Try.sol
@@ -38,6 +38,38 @@ contract Basic{
 
         }
 
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.0;
+
+// contract Contract1 {
+//     uint public value;
+
+//     function setValue(uint _value) external {
+//         value = _value;
+//     }
+    
+//     function getValue() external view returns (uint) {
+//         return value;
+//     }
+// }
+
+// contract Contract2 {
+//     Contract1 public contract1Instance;
+    
+//     constructor(address _contract1Address) {
+//         contract1Instance = Contract1(_contract1Address);
+//     }
+    
+//     function callExternalFunction(uint _newValue) external {
+//         contract1Instance.setValue(_newValue);
+//     }
+    
+//     function getExternalValue() external view returns (uint) {
+//         return contract1Instance.getValue();
+//     }
+// }
+
+
     
 
         
